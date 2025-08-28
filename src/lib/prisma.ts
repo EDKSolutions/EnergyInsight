e30c7af2078a37a45f51d9b09b0f262f8e43c9d9
Oscar López Arriaga <lopezarriagaoscar@gmail.com>
2025-08-28 16:04:37 -0600

    fix: change

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,23 +1,4 @@
-import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
-
-// Use Prisma Accelerate only in production if DATABASE_URL starts with prisma://
-const shouldUseAccelerate = process.env.NODE_ENV === 'production' && 
-  (process.env.DATABASE_URL?.startsWith('prisma://') || process.env.DATABASE_URL?.startsWith('prisma+postgres://'))
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-let prismaClient: any
-
-if (shouldUseAccelerate) {
-  prismaClient = new PrismaClient().$extends(withAccelerate())
-} else {
-  prismaClient = new PrismaClient()
-}
-
-export const prisma = globalForPrisma.prisma ?? prismaClient
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma = new PrismaClient().$extends(withAccelerate());
