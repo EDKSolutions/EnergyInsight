c04608bb2bc06d0c4bc9139a319a92a3d45af629
Max Carey <54321@protonmail.com>
2025-09-16 13:10:39 -0600

    wip

--- src/components/calculation/energy/index.tsx
+++ src/components/calculation/energy/index.tsx
@@ -4,11 +4,13 @@ import Comparation from '@/components/calculation/energy/comparation'
 import { CalculationResult } from '@/types/calculation-result-type'
 import EnergyCard from '@/components/calculation/energy/energy'
 import CostCalculation from '@/components/calculation/energy/cost-calculation'
+import EnergyCalculationBreakdown from '@/components/calculation/energy/energy-calculation-breakdown'
 
 const Energy = ({ c }: { c: CalculationResult }) => {
   return (
     <div className="flex flex-col gap-4">
       <Comparation c={c} />
+      <EnergyCalculationBreakdown c={c} />
       <CostCalculation c={c} />
       <EnergyCard c={c} />
       <Data />
